f2b892bfb1e7caa3019bd164bc74313a6b00044e
Jason Inouye <jasonjinouye@gmail.com>
2023-09-11 12:56:21 -0500

    button navigation added

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import ImageScreen from '../screens/ImageScreen';
+
+const ImageDetail = ({ imageSource, title, score }) => {
+    return <View>
+        <Image source={imageSource} />
+        <Text>{title}</Text>
+        <Text>Image Score - {score}</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation })=> {
+
   return (<View>
     <Text style={styles.text}>Are you still live?</Text>
     <Button 
-      onPress={() => console.log('Button pressed')}
+      onPress={() => navigation.navigate('Components')}
       title="Go to Components Demo"
     />
-    <TouchableOpacity onPress={() => console.log('Button pressed')}>
-      <Text>Go to List Demo</Text>
-    </TouchableOpacity>
+    <Button 
+      onPress={() => navigation.navigate('List')}
+      title="Go to List Demo"
+    />
+    <Button 
+      onPress={() => navigation.navigate('Image')}
+      title="Go to Image Demo"
+    />
   </View>)
 };
 
